47425ba08b7e641311ed8667429d8f16bb1521db
Ivan Todorov <Ivan Todorov>
2017-04-05 22:47:21 +0300

    Add Budget

--- FinanceTracker/src/model/budget/Budget.java
+++ FinanceTracker/src/model/budget/Budget.java
@@ -1,5 +1,36 @@
 package model.budget;
 
+import java.util.ArrayList;
+
+import model.budget.flows.CashFlow;
+import model.budget.flows.Expense;
+import model.budget.flows.Income;
+import model.exceptions.InvalidBudgetException;
+
 public class Budget {
 
+	private String name;
+	private ArrayList<Expense> expenses;
+	private ArrayList<Income> incomes;
+	private double balance;
+	
+	public Budget(String name) throws InvalidBudgetException{
+		if (name == null || name.isEmpty()) {
+			throw new InvalidBudgetException();
+		}
+		this.name = name;
+		this.expenses = new ArrayList<>();
+		this.incomes = new ArrayList<>();
+		this.balance = 0;
+	}
+	
+	public void addCashFlow(CashFlow flow){
+		if (flow instanceof Income) {
+			this.incomes.add((Income)flow);
+		} else {
+			this.expenses.add((Expense)flow);
+		}
+		this.balance += flow.getQuantity();
+	}
+	
 }
---
+++ FinanceTracker/src/model/exceptions/InvalidBudgetException.java
@@ -0,0 +1,3 @@
+package model.exceptions;
+
+public class InvalidBudgetException extends Exception {}
