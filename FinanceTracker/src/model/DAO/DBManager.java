f6ff27572c4cc16fa0bf6be05d0f3e59d695d37a
Ivan Todorov <Ivan Todorov>
2017-04-05 23:27:33 +0300

    Add DBManager

---
+++ FinanceTracker/src/model/DAO/DBManager.java
@@ -0,0 +1,40 @@
+package model.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBManager {
+	
+	private static DBManager db = null;
+	private Connection con = null;
+
+	private DBManager(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			if(System.getProperty("os.name").startsWith("Linux")){
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/financeTracker?autoReconnect=true&useSSL=false", "streetzaki", "14eiuqhwdyeuQ*");
+			}
+			else{
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/financeTracker?autoReconnect=true&useSSL=false", "root", "injikipliok");
+			}
+		}catch(Exception ex){
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public synchronized Connection getConnection(){
+		if(db == null){
+			getInstance();
+		}
+		return DBManager.db.con;
+	}
+	
+	public synchronized static DBManager getInstance(){
+		if(DBManager.db == null){
+			DBManager.db = new DBManager();
+		}
+		return DBManager.db;
+	}
+
+}
